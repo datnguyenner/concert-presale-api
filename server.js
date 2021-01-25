3ade99eb6a307a520a6133b16c2add6ee1d9fa03
datnguyenner <dat.nguyen@seven.me>
2021-01-25 04:38:35 -0800

    Add proxy config

--- server.js
+++ server.js
@@ -1,59 +1,29 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const bcrypt = require('bcrypt');
-const knex = require('knex');
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: '',
-        password: '',
-        database: 'events'
-    }
-});
+const fetch = require('node-fetch');
+const convert = require('xml-js');
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Server is working');
-})
-
-app.post('/signin', async (req, res) => {
-    const login = await db.select('email', 'hash').from('login').where('email', '=', req.body.email);
-    console.log(login, 'test-login');
-    if (login[0]) {
-        const isValid = bcrypt.compareSync(req.body.password, login[0].hash);
-        if (isValid) {
-            const user = await db.select('*').from('users').where('email', '=', req.body.email);
-            user[0] ? res.status(200).json(user[0]) : res.status(400).json('unable to get user');
-        }
-    } else {
-        res.status(400).json('wrong credentials')
-    }
-})
-
-app.post('/register', async (req, res) => {
-    
-    let { username, email, password } = req.body;
-    const hash = await bcrypt.hash(password, 10);
-
-    await db.transaction(async trx => {
-        const newLogin = {hash, email};
-        const loginEmail = await trx.insert(newLogin).into('login').returning('email');
-        if(loginEmail) {
-            const newUser = { username, email, created: Date.now() }
-            const id = await trx.insert(newUser).into('users').returning('id');
-            id ? res.status(201).json(id[0]) : res.status(400).json('unable to register');
-        }else{
-            res.status(400).json('unable to register');
-        }
-      })
-})
-
-app.listen(3000, () => {
-    console.log('App is running on port 3000');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
+
+app.get('/documents', async (req, res) => {
+   const url= 'https://www.patersonnj.gov/egov/api/request.egov?request=feed;dateformat=%25m-%25d-%25Y;ctype=4;title=Latest%20Documents;classificationid=62;count=200'
+   try{
+    const response = await fetch(url)
+    const body = await response.text()
+    const data = await JSON.parse(convert.xml2json(body, {compact: true, spaces: 4}))
+    const entries = data.feed.entry
+    const filteredArray = entries.filter(entry => (entry.title._text.includes('Bid') || entry.title._text.includes('RFP') || entry.title._text.includes('BID') || entry.title._text.includes('rfp')))
+    res.status(200).json(filteredArray)
+   } catch(e){
+       res.status(500).json({message: 'unable to parse data'})
+   }
+   
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
