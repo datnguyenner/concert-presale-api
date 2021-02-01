7e532aebd43e0a7c1083565dded2f1ce47b022d6
datnguyenner <dat.nguyen@seven.me>
2021-02-01 08:37:55 -0800

    Add new url

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const convert = require('xml-js');
 
 const app = express();
-
+let entries
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -11,14 +11,16 @@ app.use((req, res, next) => {
 });
 
 app.get('/documents', async (req, res) => {
-   const url= 'https://www.patersonnj.gov/egov/api/request.egov?request=feed;dateformat=%25m-%25d-%25Y;ctype=4;title=Latest%20Documents;classificationid=62;count=200'
+   const url= `https://www.patersonnj.gov/egov/api/request.egov?xml=<egov><request id="agedas" xsl="cw-docs-no-desc" type="feed"><title>Latest Documents</title><ctype>documents</ctype><classificationid>63</classificationid><classificationid>62</classificationid><count>200</count><dateformat>%A, %B %d, %Y</dateformat></request></egov>`
    try{
-    const response = await fetch(url)
-    const body = await response.text()
-    const data = await JSON.parse(convert.xml2json(body, {compact: true, spaces: 4}))
-    const entries = data.feed.entry
+     if(!entries){
+      const response = await fetch(url)
+      const body = await response.text()
+      const data = await JSON.parse(convert.xml2json(body, {compact: true, spaces: 4}))
+      entries = data.results.result.feed.entry
+     }
     const filteredArray = entries.filter(entry => (entry.title._text.includes('Bid') || entry.title._text.includes('RFP') || entry.title._text.includes('BID') || entry.title._text.includes('rfp')))
-    res.status(200).json(filteredArray)
+    res.status(200).json(filteredArray.filter(entry=> (!entry.title._text.includes('Re-Bid'))))
    } catch(e){
        res.status(500).json({message: 'unable to parse data'})
    }
