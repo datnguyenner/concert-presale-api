7862e152606f4834ea422a80d24067c406451197
datnguyenner <dat.nguyen@seven.me>
2021-02-09 11:25:10 -0800

    Add RFQ filter

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/documents', async (req, res) => {
-   const url= `https://www.patersonnj.gov/egov/api/request.egov?xml=<egov><request id="agedas" xsl="cw-docs-no-desc" type="feed"><title>Latest Documents</title><ctype>documents</ctype><classificationid>63</classificationid><classificationid>62</classificationid><count>200</count><dateformat>%A, %B %d, %Y</dateformat></request></egov>`
+   const url= `https://www.patersonnj.gov/egov/api/request.egov?xml=<egov><request id="agedas" xsl="cw-docs-no-desc" type="feed"><title>Latest Documents</title><ctype>documents</ctype><classificationid>63</classificationid><classificationid>62</classificationid><count>150</count><dateformat>%A, %B %d, %Y</dateformat></request></egov>`
    try{
      if(!entries){
       const response = await fetch(url)
@@ -19,7 +19,7 @@ app.get('/documents', async (req, res) => {
       const data = await JSON.parse(convert.xml2json(body, {compact: true, spaces: 4}))
       entries = data.results.result.feed.entry
      }
-    const filteredArray = entries.filter(entry => (entry.title._text.includes('Bid') || entry.title._text.includes('RFP') || entry.title._text.includes('BID') || entry.title._text.includes('rfp')))
+    const filteredArray = entries.filter(entry => (entry.title._text.includes('Bid') || entry.title._text.includes('RFP') || entry.title._text.includes('BID') || entry.title._text.includes('rfp') || entry.title._text.includes('RFQ') || entry.title._text.includes('rfq')))
     res.status(200).json(filteredArray.filter(entry=> (!entry.title._text.includes('Re-Bid'))))
    } catch(e){
        res.status(500).json({message: 'unable to parse data'})
